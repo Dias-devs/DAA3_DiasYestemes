9b4d5ff1b705e2a21af5507a320a1b5e5f9d5b36
Dias-devs <krasavchikcezar777@gmail.com>
2025-10-26 18:21:19 +0500

    feat(tests): Testing Kruskal, Prim and MST Validation

---
+++ src/test/java/MST/KruskalTest.java
@@ -0,0 +1,35 @@
+package MST;
+
+import MST.Algorithms.KruskalAlgorithm;
+import MST.Model.Edge;
+import MST.Model.Graph;
+import MST.Model.Output.MSTResult;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KruskalTest {
+
+    @Test
+    void kruskalProducesSameCostAsPrim() {
+        var nodes = List.of("A","B","C","D","E");
+        var edges = List.of(
+                new Edge("A","B",4),
+                new Edge("A","C",3),
+                new Edge("B","C",2),
+                new Edge("B","D",5),
+                new Edge("C","D",7),
+                new Edge("C","E",8),
+                new Edge("D","E",6)
+        );
+        Graph g = new Graph(1, nodes, edges);
+        KruskalAlgorithm kr = new KruskalAlgorithm();
+        MSTResult res = kr.findMST(g);
+
+        assertNotNull(res);
+        assertEquals(g.vertexCount() - 1, res.getMstEdges().size());
+        assertEquals(16, res.getTotalCost());
+    }
+}
